703cceb1e53e16b16fd461da0324fbffdf770b24
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-07-04 18:50:14 +0300

    первый вариант

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+export default [
+  {
+    files: 
+    ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  { 
+    languageOptions: 
+    { 
+      parserOptions: 
+      { 
+        ecmaFeatures: 
+        { 
+          jsx: true 
+        } 
+      } 
+    } 
+  },
+  {
+    languageOptions: 
+    { 
+      globals: globals.browser 
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+
+  {
+    "settings": {
+    "react": {
+    "version": "7.34.3" 
+    }
+    }
+   },
+
+   {
+   rules: {
+    "no-debugger": "off",
+    "no-multiple-empty-lines": [2, {"max": 1}]
+   }
+  }
+]
